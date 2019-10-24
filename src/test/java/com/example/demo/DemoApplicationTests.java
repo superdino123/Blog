3f51d654a564ee215f9b1246cf77c37059218218
11196 <11196@DESKTOP-FDCLQUA>
2019-10-25 01:01:17 +0800

    1.连接mysql 2.配置日志 3.测试获取数据

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.demo.entity.Article;
+import com.example.demo.init.InitService;
+import com.example.demo.service.ArticleService;
+
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
+
+	private static Logger logger = LoggerFactory.getLogger(InitService.class);
+
+	@Autowired
+	private ArticleService articleService;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void articleTest() {
+		Article a = articleService.selectArticleById(1);
+		logger.info(a.toString());
+	}
 }
